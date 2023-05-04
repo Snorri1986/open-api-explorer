0f2c5cd81f067704dbc8c422fcfeade22defa545
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-05-04 22:17:28 +0200

    Default set of annotations is ready

--- src/main/java/org/snorri1986/CommonEndpoint.java
+++ src/main/java/org/snorri1986/CommonEndpoint.java
@@ -1,20 +1,29 @@
 package org.snorri1986;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+@Tag(name = "API", description = "First documented endpoint")
 public class CommonEndpoint {
 
     /**
-     *
      * Source: <a href="https://dev.to/optnc/openapi-java-annotation-for-better-api-documentation-43oe">...</a>
      */
 
+    @ApiResponse(code = 200, message = "All good")
     @GET
+    @Operation(summary = "Hello endpoint",
+            description = "Generate hello endpoint")
     @Produces(MediaType.TEXT_PLAIN)
+    @Parameter(description = "Empty payload", example = "null")
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
