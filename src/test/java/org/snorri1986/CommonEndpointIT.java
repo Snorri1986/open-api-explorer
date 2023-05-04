8d66a0f11251274020b72c6a1da7260553d0aff6
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-05-04 21:44:57 +0200

    Generated simple Quarkus project with common endpoint

---
+++ src/test/java/org/snorri1986/CommonEndpointIT.java
@@ -0,0 +1,8 @@
+package org.snorri1986;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class CommonEndpointIT extends CommonEndpointTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ src/test/java/org/snorri1986/CommonEndpointTest.java
@@ -0,0 +1,21 @@
+package org.snorri1986;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class CommonEndpointTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from RESTEasy Reactive"));
+    }
+
+}
