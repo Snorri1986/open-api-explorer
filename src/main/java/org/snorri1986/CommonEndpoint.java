9f04cd39a25f65ce07f86797d56b6295e19e2ea6
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-05-05 14:56:41 +0200

    improve okhttp version

--- src/main/java/org/snorri1986/CommonEndpoint.java
+++ src/main/java/org/snorri1986/CommonEndpoint.java
@@ -10,7 +10,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+
+@Path("/hlelo")
 @Tag(name = "API", description = "First documented endpoint")
 public class CommonEndpoint {
 
